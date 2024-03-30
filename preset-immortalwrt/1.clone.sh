267d007ca27bc3a132a470d4b24092cc4257cd52
GruntFish <165454237+GruntFish@users.noreply.github.com>
2024-03-30 19:20:52 +0800

    Update 1.clone.sh

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -24,8 +24,6 @@ mv ./$CODE_DIR/* ./
 SUPPLY_DIR=_supply_packages
 echo "src-link supply $PWD/$SUPPLY_DIR" >> feeds.conf.default
 mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
-git clone --depth 1 https://github.com/GruntFish/luci-app-argon-config.git
 git clone --depth 1 https://github.com/Glunefish/small-package.git
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
