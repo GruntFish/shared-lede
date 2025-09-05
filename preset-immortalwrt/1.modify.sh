28fa1763cbae0bddb32021bf070458c6648337f3
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-09-05 15:31:02 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # modify login IP
-#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
 
 #sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/' feeds/luci/collections/luci/Makefile
 #sed -i 's/Bootstrap theme/Argonv3 theme/' feeds/luci/collections/luci/Makefile
