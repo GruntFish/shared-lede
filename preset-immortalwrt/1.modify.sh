c37e21b0f4919a0276d63a7be3b990aca341d9ee
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-09-06 01:07:03 +0800

    更新 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -3,11 +3,11 @@
 # modify login IP
 sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
 
-#sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/' feeds/luci/collections/luci/Makefile
-#sed -i 's/Bootstrap theme/Argonv3 theme/' feeds/luci/collections/luci/Makefile
-#sed -i 's/normal/dark/' feeds/luci/applications/luci-app-argon-config/root/etc/config/argon
+#sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/Bootstrap theme/Argonv3 theme/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/normal/dark/g' feeds/luci/applications/luci-app-argon-config/root/etc/config/argon
 
-sed -i 's/0x2B2/0x312/' target/linux/ramips/patches-5.4/322-mt7621-fix-cpu-clk-add-clkdev.patch
+sed -i 's/0x2B2/0x312/g' target/linux/ramips/patches-5.4/322-mt7621-fix-cpu-clk-add-clkdev.patch
 
 
 # replace geodata source
