ac88e452faa4d10f9a7fe9802e75177d834d8194
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-09-05 09:08:16 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -3,9 +3,9 @@
 # modify login IP
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/' feeds/luci/collections/luci/Makefile
-sed -i 's/Bootstrap theme/Argonv3 theme/' feeds/luci/collections/luci/Makefile
-sed -i 's/normal/dark/' feeds/luci/applications/luci-app-argon-config/root/etc/config/argon
+#sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/' feeds/luci/collections/luci/Makefile
+#sed -i 's/Bootstrap theme/Argonv3 theme/' feeds/luci/collections/luci/Makefile
+#sed -i 's/normal/dark/' feeds/luci/applications/luci-app-argon-config/root/etc/config/argon
 
 # replace geodata source
 . $(dirname $0)/../extra-files/update-geodata.sh
