96436c612ea2dfa14307ec34e4567d8d5be18f63
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-09-11 23:40:21 +0800

    Update 1.clone.sh

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -3,7 +3,7 @@
 # download base code
 CODE_DIR=_firmware_code
 CODE_URL=https://github.com/GruntFish/immortalwrt.git
-CODE_BRANCH=master
+CODE_BRANCH=openwrt-23.05
 SWITCH_LATEST_TAG=false
 git clone --single-branch -b $CODE_BRANCH $CODE_URL $CODE_DIR
 if $SWITCH_LATEST_TAG; then
