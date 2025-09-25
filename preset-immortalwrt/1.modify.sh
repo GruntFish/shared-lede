2268653db6562a2720f8c2c9a8d8175c584be305
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-09-25 17:10:51 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -5,7 +5,14 @@ sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_gene
 
 #sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/g' feeds/luci/collections/luci/Makefile
 #sed -i 's/Bootstrap theme/Argonv3 theme/g' feeds/luci/collections/luci/Makefile
-sed -i 's/normal/dark/g' feeds/luci/applications/luci-app-argon-config/root/etc/config/argon
+#sed -i 's/normal/dark/g' feeds/luci/applications/luci-app-argon-config/root/etc/config/argon
+
+sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom-ng/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom-ng/g' feeds/luci/collections/luci-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom-ng/g' feeds/luci/collections/luci-ssl-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom-ng/g' feeds/luci/collections/luci-light/Makefile
+sed -i 's/Bootstrap theme/infinityfreedom theme/g' feeds/luci/collections/luci/Makefile
+
 
 sed -i 's/0x2B2/0x312/g' target/linux/ramips/patches-5.4/102-mt7621-fix-cpu-clk-add-clkdev.patch
 #sed -i 's/0x2B2/0x312/g' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
