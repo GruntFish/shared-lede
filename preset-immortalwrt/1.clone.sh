2794322225e1328d2aa62b7828d42b3a1fd8d368
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-09-28 22:20:33 +0800

    Update 1.clone.sh

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -27,7 +27,7 @@ mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
 
 git clone https://github.com/lwb1978/openwrt-gecoosac ./package/openwrt-gecoosac
 git clone https://github.com/nikkinikki-org/OpenWrt-momo ./package/momo
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome ./package/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome ./package/luci-app-adguardhome
 
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
