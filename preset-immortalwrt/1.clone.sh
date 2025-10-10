fa3821e58241d86afd486c2f838a616c415ed681
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-10 11:03:47 +0800

    Update 1.clone.sh

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -21,20 +21,3 @@ if $SWITCH_LATEST_TAG; then
     cd ..
 fi
 mv ./$CODE_DIR/* ./
-
-# download app codes
-SUPPLY_DIR=_supply_packages
-echo "src-link supply $PWD/$SUPPLY_DIR" >> feeds.conf.default
-mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
-
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom ./package/luci-theme-infinityfreedom
-git clone https://github.com/lwb1978/openwrt-gecoosac ./package/openwrt-gecoosac
-git clone https://github.com/zzsj0928/luci-app-pushbot ./package/luci-app-pushbot
-
-git clone https://github.com/GruntFish/OP-Packages ./package/OP-Packages
-git clone https://github.com/sirpdboy/luci-app-lucky ./package/luci-app-lucky
-git clone https://github.com/sirpdboy/luci-app-watchdog ./package/luci-app-watchdog
-
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git && mv openwrt-passwall2/luci-app-passwall2 ./ && rm -rf openwrt-passwall2
