2bd2477e711e1a6a77808e27933d74eca09911a4
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-10 10:49:35 +0800

    Update del.sh

--- preset-immortalwrt/del.sh
+++ preset-immortalwrt/del.sh
@@ -1,4 +1,8 @@
-rm -rf ./feeds/packages/net/adguardhome
-
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+
+
+rm -rf ./feeds/packages/net/adguardhome
+mkdir -p ./feeds/packages/net/adguardhome
+mv ./temp-op/adguardhome/* ./feeds/packages/net/adguardhome/
+rm -rf ./temp-op
